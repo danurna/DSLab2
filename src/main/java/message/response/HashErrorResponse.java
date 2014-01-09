e8e0e8a453c6545bc6a98b5bbcc7301582e6311b
Felix Ledóchowski <flm@live.at>
2014-01-09 13:19:27 +0100

    forgot file, sorry!

---
+++ src/main/java/message/response/HashErrorResponse.java
@@ -0,0 +1,17 @@
+package message.response;
+
+import message.Response;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Felix
+ * Date: 09.01.14
+ * Time: 12:38
+ * To change this template use File | Settings | File Templates.
+ */
+public class HashErrorResponse implements Response {
+    @Override
+    public String toString() {
+        return "Mismatched Hash";    //To change body of overridden methods use File | Settings | File Templates.
+    }
+}
